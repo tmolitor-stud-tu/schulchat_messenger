88b08e09c990a541822823445cc44d0bf80c81cc
Thilo Molitor <thilo@eightysoft.de>
2020-02-06 02:33:21 +0100

    Fix kotlin bug while initializing fcm

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,7 +4,8 @@
 
 ./gradlew assemblePlaystoreRelease $@ || exit 1
 real_apk="$(realpath $apk)"
-jarsigner -keystore "$ORG_GRADLE_PROJECT_mStoreFile" -storepass "$ORG_GRADLE_PROJECT_mStorePassword" -keypass "$ORG_GRADLE_PROJECT_mKeyPassword" "$real_apk" "$ORG_GRADLE_PROJECT_mKeyAlias" && $ANDROID_HOME/build-tools/28.0.3/zipalign -f 4 "$real_apk" "$(basename "$real_apk")" && adb install -r "$(basename "$real_apk")" && (
+dest="$(basename "$real_apk" | sed 's/-unsigned/-signed/g')"
+jarsigner -keystore "$ORG_GRADLE_PROJECT_mStoreFile" -storepass "$ORG_GRADLE_PROJECT_mStorePassword" -keypass "$ORG_GRADLE_PROJECT_mKeyPassword" "$real_apk" "$ORG_GRADLE_PROJECT_mKeyAlias" && $ANDROID_HOME/build-tools/28.0.3/zipalign -f 4 "$real_apk" "$dest" && adb install -r "$dest" && (
 	sleep 1
 	trap "adb shell am force-stop $app & exit" INT
 	adb shell am start -n $app/$activity &
